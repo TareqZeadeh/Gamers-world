3fe8e83458e1df6840913fa94823091122c04d26
TareqZeadeh <>
2021-05-06 07:30:57 +0000

    checking the code

--- app.js
+++ app.js
@@ -2,23 +2,20 @@ alert('Welcome in our Website');
 
 var faivoretgame=prompt('what do you like more pubg or cod ?');
 
+
 if(faivoretgame =='pubg'){
-
-var x=prompt('what do you like more pubg or cod ?');
-
-if(x =='pubg'){
 
   document.write('<img src="https://www.91-cdn.com/hub/wp-content/uploads/2021/04/pubg-lite-shut-down-image.jpg"  width="300px">');
 
-  document.write('<a href="https://www.pubg.com/ar/">download it here </a>')
+  document.write('<a href="https://www.pubg.com/ar/">download it here </a>');
 
 
-}else if( faivoretgame === 'cod'){
 
-}else if(x === 'cod'){
+
+}else if(faivoretgame == 'cod'){
 
   document.write('<img src="https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blog/hero/mw-wz/WZ-Season-Three-Announce-TOUT.jpg" width="300px">');
-   document.write('<a href="https://www.callofduty.com/ar/blog/2021/04/Warzone-Season-Three-Overview-Verdansk-84">download it here </a>')
+   document.write('<a href="https://www.callofduty.com/ar/blog/2021/04/Warzone-Season-Three-Overview-Verdansk-84">download it here </a>');
 
 }else{
   document.write('<h3> please only write pubg or cod </h3>');
@@ -29,13 +26,10 @@ if(x =='pubg'){
 var likefreefire = confirm("Do you like free fire");
 if (likefreefire == true) {
 
-var r = confirm("Do you like free fire");
-if (r == true) {
-
 
   document.write('<img src="https://t3me.com/ar/wp-content/uploads/sites/3/2020/12/FF.jpg" width="300px">');
-   document.write('<a href="https://free-fire-battlegrounds.ar.uptodown.com/android/download">download it here for mobile </a>')
+   document.write('<a href="https://free-fire-battlegrounds.ar.uptodown.com/android/download">download it here for mobile </a>');
   
 } else {
- alert('well ... pubg and cod is more Enthusiastic Battlefield games ... Good Luck'); ;
+ alert('well ... pubg and cod is more Enthusiastic Battlefield games ... Good Luck'); 
 }
